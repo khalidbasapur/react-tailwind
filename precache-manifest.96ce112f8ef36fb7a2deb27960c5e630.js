5009f661d148309c1826571f5b67055a664cbc00
khalidbasapur <khalid.basapur@gmail.com>
2020-08-20 12:42:17 +0400

    Updates

---
+++ precache-manifest.96ce112f8ef36fb7a2deb27960c5e630.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f692a8e477ed1cd76a9f6cb50924e5a9",
+    "url": "/react-tailwind/index.html"
+  },
+  {
+    "revision": "f145f44ea6e5a0b68039",
+    "url": "/react-tailwind/static/css/main.767097fd.chunk.css"
+  },
+  {
+    "revision": "337d0558e53789d6d3b2",
+    "url": "/react-tailwind/static/js/2.f33d654a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tailwind/static/js/2.f33d654a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f145f44ea6e5a0b68039",
+    "url": "/react-tailwind/static/js/main.6e8ec54b.chunk.js"
+  },
+  {
+    "revision": "8edfd4cf7573c2f74d7c",
+    "url": "/react-tailwind/static/js/runtime-main.ef017eee.js"
+  },
+  {
+    "revision": "5d206f952259ef1a0abf107d2b5793b4",
+    "url": "/react-tailwind/static/media/profile.5d206f95.jpg"
+  }
+]);
